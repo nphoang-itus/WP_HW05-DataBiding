c7ec7a631cda980a7ecf845910ad012ba74ce61d
nphoang <nphuchoang.itus@gmail.com>
2025-10-24 14:33:05 +0700

    Feat: Create MainViewModel to load mobile phone data

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,6 +13,12 @@ using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
+using System.Collections.Generic; // Cần cho List<T>
+
+using DataBiding.Model;
+using DataBiding.DataAccess;
+using DataBiding.Utils;
+
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -23,6 +29,31 @@ namespace DataBiding
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        // === PHẦN 1: TẠO LỚP VIEWMODEL LỒNG BÊN TRONG ===
+        // Lớp này chịu trách nhiệm chuẩn bị dữ liệu cho UI
+        public class MainViewModel
+        {
+            // Một thuộc tính public để chứa danh sách điện thoại.
+            // UI sẽ binding vào thuộc tính này.
+            public List<MobilePhone> MobilePhones { get; set; } = new();
+
+            // Lấy repository thông qua Dependency Injection
+            private IRepository<MobilePhone> _repo =
+                Services.Get<IRepository<MobilePhone>>();
+
+            // Hàm khởi tạo của ViewModel
+            public MainViewModel()
+            {
+                // Nạp dữ liệu từ repository vào danh sách
+                MobilePhones = _repo.GetAll();
+            }
+        }
+
+        // === PHẦN 2: TẠO MỘT INSTANCE CỦA VIEWMODEL ===
+        // MainWindow sẽ sở hữu một đối tượng ViewModel.
+        // XAML sẽ truy cập vào đây để lấy dữ liệu.
+        public MainViewModel ViewModel { get; set; } = new();
+
         public MainWindow()
         {
             InitializeComponent();
